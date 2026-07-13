d66b5255cb5290e25607e6d17649805ae0a0437f
Takayoshi Otake <takayoshiotake@users.noreply.github.com>
2026-07-13 09:45:36 +0900

    chore(dev): add Windows development run script

---
+++ scripts/run-dev.ps1
@@ -0,0 +1,22 @@
+$ErrorActionPreference = "Stop"
+
+$env:OIDC_ISSUER = "http://localhost:8081/realms/dev"
+$env:OIDC_BROWSER_ISSUER = "http://localhost:8081/realms/dev"
+$env:OIDC_CLIENT_ID = "shiroyagi"
+$env:OIDC_CLIENT_SECRET_FILE = "secrets/dev/oidc_client_secret"
+$env:OIDC_REDIRECT_URI = "http://localhost:8080/auth/callback"
+
+$env:DATABASE_HOST = "localhost"
+$env:DATABASE_PORT = "5432"
+$env:DATABASE_NAME = "shiroyagi"
+$env:DATABASE_USER = "shiroyagi"
+$env:DATABASE_PASSWORD_FILE = "secrets/dev/postgres_password"
+
+$env:MAIL_ACCOUNT_KEK_FILE = "secrets/dev/mail_account_kek"
+$env:MAIL_ACCOUNT_KEK_VERSION = "1"
+
+go run ./cmd/shiroyagi @args
+
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
